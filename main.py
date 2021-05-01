f01efa4fa524dbb2bac225e0c9a19ef1f89ee94b
jh070 <jonardonhazarika7@gmail.com>
2021-05-01 23:33:15 +0530

    Changes and Cleanup

--- main.py
+++ main.py
@@ -25,28 +25,7 @@ def get_text():
     text = text[:-2]
     text = " ".join(text.split("\n"))
     
-    ''' more refining of text '''
-
-    if '|' in text:
-        text = text.replace('|','I')
-    else:
-        pass
     
-    if '1' in text:
-        text = text.replace('1','I')
-    else:
-        pass
-
-    test_text = text.split()
-    
-    if "{" or "}" or "|" or "(" or ")" in test_text:
-        
-        for k in test_text:
-            if k == "{" or k=="}" or k=="|" or k=="(" or k==")":
-                test_text.remove(k)
-        
-    text = " ".join(test_text)
-
     ''' finally return the text '''
     return (text + " ")
 
@@ -61,24 +40,7 @@ def start_typing(text):
         to consider as valid input
     '''
 
-    pyg.write(text,interval=0.0245)
-    
-def waiting_period():
-    ''' now after capturing the
-        screenshot the program will 
-        start typing it automatically which
-        we don't want as the browser window
-        may be out of focus, so we will
-        add some waiting period for the 
-        program and in the mean time we will
-        re focus on the browser
-    '''
-    os.system('cls')
-    time.sleep(0.33)
-    '''for k in [3,2,1]:
-        print(k)
-        time.sleep(1.125)
-        os.system('cls')'''
+    pyg.write(text,interval=0.0545)
     
 
 def time_to_quit(end):
@@ -98,7 +60,7 @@ def main():
         end = start.replace(minute=start.minute+1)
         
     k = input("Start? y/n: ").strip()
-    time.sleep(2.1)
+    time.sleep(2.5)
     if k.lower()=='y':
         
         while True:
